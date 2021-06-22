a9b279b59622525b81bdee46c33c058293a0d368
urte <janeliunaite.urte@gmail.com>
2021-06-22 13:45:46 +0300

    while loop changed to do...while loop

--- while-loop.js
+++ while-loop.js
@@ -1,9 +1,23 @@
 var myArray = [];
-
 var i = 5;
 while(i >= 0) {
   myArray.push(i); // adds members to the array
   i--; // -> -1
 }
-
 console.log(myArray);
+
+// do...while loop:
+var myArray = [];
+var i = 10;
+while (i < 5) {
+  myArray.push(i);
+  i++;
+}
+
+// CHANGED TO:
+var myArray = [];
+var i = 10;
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5);
